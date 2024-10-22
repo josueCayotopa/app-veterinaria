0711a522c73ca15d73e91fc44c3816669e37abbb
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-22 13:09:15 -0500

    modificacion de recurso medico veterinario

--- app/Filament/Resources/MedicoVeterinarioResource.php
+++ app/Filament/Resources/MedicoVeterinarioResource.php
@@ -15,12 +15,13 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Validation\Rule;
 
 class MedicoVeterinarioResource extends Resource
 {
     protected static ?string $model = MedicoVeterinario::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
 
     public static function form(Form $form): Form
     {
@@ -29,15 +30,65 @@ class MedicoVeterinarioResource extends Resource
                 Group::make()->schema([
                     Section::make("Información Básica")->schema([
                         Forms\Components\Select::make('tipo_documento')
+                            ->label('Documento de Identidad')
                             ->options([
                                 'DNI' => 'DNI',
                                 'PASAPORTE' => 'Pasaporte',
                                 'CARNET DE EXTRANJERIA' => 'Carnet de extranjería'
                             ])
-                            ->required(),
+                            ->required()
+                            ->reactive(),
                         Forms\Components\TextInput::make('numero_documento')
                             ->required()
-                            ->maxLength(255),
+                            ->label('Numero de Documento')
+                            ->maxLength(255)
+                            ->rule(
+                                function (callable $get) {
+                                    $tipoDocumento = $get('tipo_documento');
+                                    if ($tipoDocumento === 'DNI') {
+                                        return [
+                                            'digits:8',
+                                            'numeric',
+                                        ];
+                                    } elseif ($tipoDocumento === 'PASAPORTE') {
+                                        return [
+                                            'string', // Debe ser un string
+                                            'min:12', // Longitud mínima
+                                            'max:12', // Longitud máxima
+                                            'regex:/^[a-zA-Z0-9]+$/', // Asegurarse que sea alfanumérico
+                                        ];
+                                    } elseif ($tipoDocumento === 'CARNET DE EXTRANJERIA') {
+                                        return [
+                                            'string', // Debe ser un string
+                                            'min:12', // Longitud mínima
+                                            'max:12', // Longitud máxima
+                                            'regex:/^[a-zA-Z0-9]+$/', // Asegurarse que sea alfanumérico
+                                        ];
+                                    }
+                                    return '';
+                                },
+                            )
+                            ->helperText(function (callable $get) {
+                                $tipoDocumento = $get('tipo_documento');
+                                if ($tipoDocumento === 'DNI') {
+                                    return 'El DNI debe tener 8 dígitos.';
+                                } elseif ($tipoDocumento === 'PASAPORTE') {
+                                    return 'El pasaporte debe tener 12 digitos.';
+                                } elseif ($tipoDocumento === 'CARNET DE EXTRANJERIA') {
+                                    return 'El carnet de extranjería debe tener 12 digitos.';
+                                }
+                                return '';
+                            })
+                            ->unique(ignoreRecord: true)
+                            ->reactive()
+                            ->validationMessages([
+                                'unique' => 'El número de documento ya ha sido registrado.',
+                                'digits' => 'El número de documento debe tener :digits dígitos.a',
+                                'numeric' => 'El número de documento debe ser solo numérico.',
+                                'regex' => 'El número de documento debe ser alfanumérico (letras y números).',
+                                'min' => 'El número de documento debe tener al menos :min caracteres.',
+                                'max' => 'El número de documento no puede tener más de :max caracteres.',
+                            ]),
                         Forms\Components\TextInput::make('nombres')
                             ->required()
                             ->maxLength(255),
@@ -45,18 +96,28 @@ class MedicoVeterinarioResource extends Resource
                             ->maxLength(255)
                             ->required(),
                         Forms\Components\TextInput::make('celular')
+
+                            ->label('Número de Celular')
                             ->maxLength(255),
                         Forms\Components\TextInput::make('email')
+                            ->label('Correo Electrónico')
                             ->email()
+                            ->unique(ignoreRecord: true)
+                            ->validationMessages([
+                                'unique' => 'El email ya ha sido registrado.',
+                            ])
+                            ->reactive()
                             ->maxLength(255)
                             ->columnSpan(2),
-                        Forms\Components\TextInput::make('direccion')
+                        Forms\Components\Textarea::make('direccion')
                             ->maxLength(255)
                             ->columnSpan(2)
                     ])
-                        ->columns(),
+                        ->columns()
+                        ->icon('heroicon-m-bars-3-bottom-left'),
 
                 ]),
+
 
                 Group::make()->schema([
                     Section::make("Informacion Profesional")->schema([
@@ -72,7 +133,8 @@ class MedicoVeterinarioResource extends Resource
                             ->maxLength(255)
                             ->columnSpan(2),
                     ])
-                        ->columns(2),
+                        ->columns(2)
+                        ->icon('heroicon-m-academic-cap'),
 
 
                 ]),
@@ -81,6 +143,8 @@ class MedicoVeterinarioResource extends Resource
                         ->maxLength(255),
                     Forms\Components\Toggle::make('disponibilidad')
                         ->label('Disponibilidad')
+                        ->onIcon('heroicon-m-bolt')
+                        ->offIcon('heroicon-m-user')
                         ->default(false),
                     Forms\Components\Select::make('tipo_contrato')
                         ->options([
@@ -89,6 +153,7 @@ class MedicoVeterinarioResource extends Resource
                         ])
                 ])
                     ->columns(3)
+                    ->icon('heroicon-m-phone-arrow-up-right'),
             ]);
     }
 
