f3790b23a9ddf684499e7fcaf3f61ea022a0497c
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-22 13:09:48 -0500

    lang = es

---
+++ lang/vendor/filament/es/components/validation.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'unique' => 'El :attribute ya ha sido registrado.',
+    'dni' => 'El :attribute debe tener 8 digitos',
+    'attributes' => [
+        'numero_documento' => 'número de documento',
+        'validacion_dni' => 'dni'
+    ],
+];
